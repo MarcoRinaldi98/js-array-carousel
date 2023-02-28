a10c78f8b2559897042df61513aac67220315877
Marco Rinaldi <marcorinaldi98@libero.it>
2023-02-28 14:01:29 +0100

    genero tramite javascript tutte le immagini

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 /*
     JAVASCRIPT
 */
+//definisco le variabili
+//array
+const imagesArray = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'   
+];
 
+const imageListDom = document.querySelector('.image-list'); 
+
+let sliderContent = "";
+
+//ciclo per aggiungere un div per ogni immagine contenuta nell'array
+for (let i = 0; i < imagesArray.length; i++) {
+    const newImageWrapper = `<div class="image-wrapper">
+                                <img class="image" src="${imagesArray[i]}" />
+                             </div>`;
+
+    
+    sliderContent += newImageWrapper;
+}
+//trasferisco la lista di tutti i div prodotti nel container nell'html
+imageListDom.innerHTML = sliderContent;
+console.log(imageListDom);
